22dd58689d7e575016570f56cba52ffdeb6d8430
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-04-17 17:01:50 -0300

    ajuste no tamanho da pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import NossosProfissionais from "@/components/profissional/NossosProfissionais";
 
 export default function Landing() {
   return (
-   <div className="flex flex-col gap-10 flex-colcontainer">
+   <div className="flex flex-col gap-10 container">
       <h1 className="text-6xl">Landing Page</h1>
       <NossosServicos />
       <NossosProfissionais />

697e2220fa6acfbcf431e30e19ccc69402686780
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-04-26 12:57:16 -0300

    incluindo o titulo nas seções

--- src/components/profissional/NossosProfissionais.tsx
+++ src/components/profissional/NossosProfissionais.tsx
@@ -1,12 +1,20 @@
 import profissionais from "@/data/contants/profissional";
 import ItemProfissional from "./ItemProfissional";
+import TituloSecao from "@/shared/TituloSecao";
 
 export default function NossosProfissionais() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-      {profissionais.map((profissional) => (
-        <ItemProfissional key={profissional.id} profissional={profissional} />
-      ))}
+    <div className="flex flex-col gap-y-16">
+      <TituloSecao
+        tag="Time"
+        principal="Nossos Brutos"
+        secundario="Só os mais brabos estão aqui! Temos o orgulho de ter o time mais qualificaddo do Brasil"
+      />
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+        {profissionais.map((profissional) => (
+          <ItemProfissional key={profissional.id} profissional={profissional} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/servico/NossosServicos.tsx
+++ src/components/servico/NossosServicos.tsx
@@ -1,13 +1,21 @@
 import servicos from "@/data/contants/servicos";
 import ItemServico from "./ItemServico";
 import Servico from "@/core/servico/Servico";
+import TituloSecao from "@/shared/TituloSecao";
 
 export default function NossosServicos() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-      {servicos.map((servico: Servico) => (
-        <ItemServico key={servico.id} servico={servico} />
-      ))}
+    <div className="flex flex-col gap-y-16">
+      <TituloSecao
+        tag="Serviços"
+        principal="Do Classico ao Rock"
+        secundario="Cabelo afiado, barba de lenhador e mãos de motoqueiro, tudo ao de rock pesado"
+      />
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+        {servicos.map((servico: Servico) => (
+          <ItemServico key={servico.id} servico={servico} />
+        ))}
+      </div>
     </div>
   );
 }

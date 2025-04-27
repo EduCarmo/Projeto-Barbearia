76be871344c160f5e1b504266d032ead9306598b
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-04-27 16:43:41 -0300

    ajustes do slogan

--- src/components/leading/Slogan.tsx
+++ src/components/leading/Slogan.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Cabecalho from "../shared/Cabecalho";
+import Link from "next/link";
 
 export default function Slogan() {
   return (
@@ -18,12 +19,28 @@ export default function Slogan() {
     "
       >
         <Cabecalho />
-        <div className="flex flex-col justify-center items-center flex-1">
+        <div className="flex flex-col justify-center items-center flex-1 gap-5">
           <h1 className="flex flex-col items-center">
-            <span className="font-thin lg:text-5xl md:text-4xl sm:text-3xl text-2xl">Transformações</span>
-            <span className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl font-black text-gradient">Lendárias</span>
+            <span className="font-thin lg:text-5xl md:text-4xl sm:text-3xl text-2xl tracking-wider">
+              Transformações
+            </span>
+            <span className="lg:text-8xl md:text-7xl sm:text-6xl text-5xl font-black text-gradient">
+              Lendárias
+            </span>
           </h1>
-          <p className="text-base sm:text-lg font-extralight">🤘 Seu estilo é o nosso rock! 🤘</p>
+          <p className="text-base sm:text-lg font-extralight">
+            🤘 Seu estilo é o nosso rock! 🤘
+          </p>
+          <Link
+            href="/agendamento"
+            className="
+            bg-gradient-to-r from-green-500 to-green-600 
+            font-semibold text-base md:text-lg px-4 py-2 text-white 
+            rounded-md hover:bg-gradient-to-r hover:from-green-600 hover:to-green-700 
+            transition duration-300 ease-in-out mt-4"
+          >
+            Agendar Agora
+          </Link>
         </div>
       </div>
     </div>

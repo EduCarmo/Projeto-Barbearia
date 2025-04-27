91f448bfb53984c70929b56131cd0aeb7c3010da
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-04-27 16:13:13 -0300

    feito o cabecalho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import ItemServico from "../components/servico/ItemServico";
 import NossosProfissionais from "@/components/profissional/NossosProfissionais";
 import SecaoBackground from "@/components/shared/SecaoBackground";
 import Rodape from "@/components/shared/Rodape";
+import Cabecalho from "@/components/shared/Cabecalho";
 
 export default function Landing() {
   return (
     <div className="flex flex-col">
+      <Cabecalho />
       <div className="container py-10">
         <h1 className="text-6xl">Landing Page</h1>
       </div>
